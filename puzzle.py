787006beace414f2e309964e719bf3387d01406c
Justin <jengrung2255@gmail.com>
2022-10-20 01:49:18 -0700

    Add puzzle get child function and swap function

--- puzzle.py
+++ puzzle.py
@@ -1,3 +1,5 @@
+import numpy    # Help to handle array
+
 
 class pazzle:
     def __init__(self, customMatrix):
@@ -7,13 +9,60 @@ class pazzle:
         for row in self.matrix:
             print(row)
             
-            
-puzzle1=[[1,2,3],
-         [4,5,6],
-         [7,8,0]]
+    # Return an array of all the possible child from the current matrix
+    def getChild(self):
+        
+        # Set x and y for 0's position
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[i])):
+                if self.matrix[i][j] == 0:
+                    x, y = i, j
+                    break
 
-puzzle2=[[1,2,3],
+        # Swap 2 number then return a new matrix
+        def swap(x1,y1,x2,y2):
+            temp = self.matrix.copy()
+            temp[x1][y1], temp[x2][y2] = temp[x2][y2], temp[x1][y1]
+            return temp
+        
+        # Check all 4 childs and add to the array if psosible
+        child = []
+        if x+1 < len(self.matrix):
+            child.append(swap(x, y, x+1, y))
+        if x-1 >= 0:
+            child.append(swap(x, y, x-1, y))
+        if y+1 < len(self.matrix[x]):
+            child.append(swap(x, y, x, y+1))
+        if y-1 >= 0:
+            child.append(swap(x, y, x, y-1))
+
+        return child
+
+    
+    def get0(self):
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[i])):
+                if self.matrix[i][j] == 0:
+                    return [i,j]
+            
+            
+puzzle1=numpy.array(
+         [[1,2,3],
+         [4,5,6],
+         [7,8,0]])
+
+puzzle2=numpy.array(
+         [[1,2,3],
          [4,0,6],
-         [7,5,8]]
-p1 = pazzle(puzzle1)
+         [7,5,8]])
+
+p1 = pazzle(puzzle2)
 p1.printMatrix()
+child = p1.getChild()
+
+print()
+
+for c in child:
+    for r in c:
+        print(r)
+    print()
