84bcf988bd4976b2023089f5c87e77f230128869
Justin <jengrung2255@gmail.com>
2022-10-24 23:51:34 -0700

    Adding A star misplaced tile function

--- puzzle.py
+++ puzzle.py
@@ -1,4 +1,3 @@
-from gettext import find
 import time
 import numpy    # Help to handle array
 
@@ -6,6 +5,7 @@ class puzzle:
     def __init__(self, customMatrix):
         self.matrix = customMatrix
         self.parent = None
+        self.height = 0
         self.cost = 0
         
     def printMatrix(self):
@@ -37,7 +37,7 @@ class puzzle:
             
             # Update path and cost for new child
             newChild.parent = self
-            newChild.cost = self.cost + 1
+            newChild.height = self.height + 1
             
             child.append(newChild)
             
@@ -47,7 +47,7 @@ class puzzle:
             
             # Update path and cost for new child
             newChild.parent = self
-            newChild.cost = self.cost + 1
+            newChild.height = self.height + 1
             
             child.append(newChild)
            
@@ -57,7 +57,7 @@ class puzzle:
             
             # Update path and cost for new child
             newChild.parent = self
-            newChild.cost = self.cost + 1
+            newChild.height = self.height + 1
             
             child.append(newChild)
             
@@ -67,7 +67,7 @@ class puzzle:
             
             # Update path and cost for new child
             newChild.parent = self
-            newChild.cost = self.cost + 1
+            newChild.height = self.height + 1
             
             child.append(newChild)
 
@@ -85,7 +85,7 @@ class puzzle:
         return self.matrix
 
 
-def uniformCostSearch(start, goal):
+def uniform_cost_search(start, goal):
     
     # Check if the start and goal are the same
     if start.checkGoalState(goal):
@@ -125,6 +125,63 @@ def uniformCostSearch(start, goal):
             # If child not in seen, Add the child to the queue
             if c.getMatrix().tolist() not in seen:
                 queue.append(c)
+
+# Helper function to calculate the misplaced tiles
+def misplaced_tiles(matrix, goal):
+    count = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] != goal[i][j]:
+                count += 1
+    return count
+
+def a_star_with_misplaced_tiles(start, goal):
+    
+    # Check if the start and goal are the same
+    if start.checkGoalState(goal):
+        return start
+    
+    # Create a queue to store all the possible path
+    queue = []
+    queue.append(start)
+    
+    # An array to keep track of all the visited matrix
+    seen = []
+    
+    # Loop until the queue is empty
+    while(True):
+        # Check if queue is empty, then return failure
+        if len(queue) == 0:
+            return False
+        
+        # Get the first element in the queue
+        current = queue.pop(0)
+        
+        # dynamic print the seen list size
+        print("Seen: " + str(len(seen)) + " Queue: " + str(len(queue)), end="\r")
+
+        # Add current to seen, convert numpy array to list to be able to compare
+        seen.append(current.getMatrix().tolist())
+        
+        # Get all the child of the current matrix
+        child = current.getChild()
+        
+        # Loop through all the child
+        for c in child:
+            # Check if the child is the same as goal
+            if c.checkGoalState(goal):
+                return c
+        
+            # If child not in seen, Add the child to the queue with the cost
+            if c.getMatrix().tolist() not in seen:
+                # Calculate the cost of the child base on misplaced tiles
+                c.cost = misplaced_tiles(c.getMatrix(), goal)
+                queue.append(c)
+                
+        # Sort the queue by cost
+        queue = sorted(queue, key=lambda x: x.cost, reverse=False)
+    
+
 
 # Goal state            
 goal=numpy.array(
@@ -181,12 +238,31 @@ puzzleX=numpy.array(
          [5,3,0],
          [7,8,6]])
 
-p1 = puzzle(puzzle16)
+p1 = puzzle(puzzle24)
 p1.printMatrix()
 print()
 
+# # Tester for uniform cost search
+# start = time.time()
+# final = uniform_cost_search(p1, goal)
+# end = time.time()
+# print(f"Finish! Taking {end - start} seconds \n \n")
+
+# print(f"COST: {final.height}")
+
+# path = []
+# while(final.parent != None):
+#     path.append((final.getMatrix(), final.height))
+#     final = final.parent
+
+# path.reverse()
+# for p in path:
+#     print(f"Matrix: \n {p[0]} \nCost: {p[1]} \n")
+    
+    
+# Tester for A* with misplaced tiles
 start = time.time()
-final = uniformCostSearch(p1, goal)
+final = a_star_with_misplaced_tiles(p1, goal)
 end = time.time()
 print(f"Finish! Taking {end - start} seconds \n \n")
 
@@ -195,7 +271,6 @@ print(f"COST: {final.cost}")
 path = []
 while(final.parent != None):
     path.append((final.getMatrix(), final.cost))
-    print()
     final = final.parent
 
 path.reverse()
